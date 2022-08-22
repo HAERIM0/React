3b7f0bcb8b5514fbfd6a2f491af1aacb699be060
HAERIM0 <HAERIM060920@gmail.com>
2022-08-22 17:54:43 +0900

    Movie

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import App from './App';
 import Coin from './Coin';
+import Movie from './Movie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Coin />
+    {/* <Coin /> */}
+    <Movie />
 
   </React.StrictMode>
 
