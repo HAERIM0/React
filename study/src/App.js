95961610ac26cc49c3bde1335dcc2b1654b18c0a
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-08-25 21:08:57 +0900

    State

--- study/src/App.js
+++ study/src/App.js
@@ -1,10 +1,14 @@
 import Composition from './components/Composition';
 import './App.css';
 import Extraction from './components/Extraction/Extraction';
+import ClassComponent from './components/State/ClassComponent';
+import FunctionalComponent from './components/State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
+      <ClassComponent />
+      <FunctionalComponent />
       <Composition />
       <Extraction />
 
