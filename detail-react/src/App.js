de1024f3654a30aaa3097c808fb52003063bcaec
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-09-09 00:20:13 +0900

    props

--- detail-react/src/App.js
+++ detail-react/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Composition from './components/props/Composition';
+import Extraction from './components/props/Extraction/Extraction';
 
 function App() {
   return (
     <div className="App">
       <Composition />
+      <Extraction />
 
     </div>
   );
