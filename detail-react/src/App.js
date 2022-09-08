1931afc419e72a6b58d74da7fc210c1473699f5e
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-09-09 00:52:29 +0900

    State

--- detail-react/src/App.js
+++ detail-react/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Composition from './components/props/Composition';
 import Extraction from './components/props/Extraction/Extraction';
+import ClassComponent from './components/State/ClassComponent';
+import FunctionalComponent from './components/State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
+      <FunctionalComponent />
+      <ClassComponent />
       <Composition />
       <Extraction />
 
