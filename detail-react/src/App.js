47bf8378851540353b539c284342862072fbe65d
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-09-11 01:11:25 +0900

    Hooks

--- detail-react/src/App.js
+++ detail-react/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-import ControlledComponent from './components/Form/ControlledComponent';
-import UncontrolledComponent from './components/Form/UncontrolledComponent';
+// import ControlledComponent from './components/Form/ControlledComponent';
+// import UncontrolledComponent from './components/Form/UncontrolledComponent';
+import State from './components/Hooks/State';
+import Reducer from "./components/Hooks/Reducer";
 // import Composition from './components/props/Composition';
 // import Extraction from './components/props/Extraction/Extraction';
 // import ClassComponent from './components/State/ClassComponent';
@@ -10,22 +12,21 @@ import UncontrolledComponent from './components/Form/UncontrolledComponent';
 // import Condition from './components/ConditionRendering/Condition';
 // import List from './components/List/List';
 
-
-
 function App() {
   return (
     <div className="App">
-      <UncontrolledComponent />
-      <ControlledComponent />
-      {/* <List /> */}
-      {/* <Condition /> */}
-      {/* <Event /> */}
-      {/* <ClassComponent2 /> */}
-      {/* <FunctionalComponent />
+      <Reducer />
+      <State />
+      {/* <UncontrolledComponent />
+      <ControlledComponent /> */}
+      {/* <List />
+      <Condition />
+      <Event />
+      <ClassComponent2 />
+      <FunctionalComponent />
       <ClassComponent />
       <Composition />
       <Extraction /> */}
-
     </div>
   );
 }
---
+++ detail-react/src/components/Hooks/State.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react';
+
+export default function State() {
+    const initiaCount = 0;
+    const [count, setCount] = useState(initiaCount);
+    return (
+        <div>
+            Count:{count}
+            <button onClick={() => setCount(initiaCount)}>Reset</button>
+            <button onClick={() => setCount(prev => prev - 1)}>-</button>
+            <button onClick={() => setCount(prev => prev + 1)}>+</button>
+        </div>
+    )
+}
