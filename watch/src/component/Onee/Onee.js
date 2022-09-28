2aa831d32147472832b3455274cb876744801507
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-09-28 22:55:11 +0900

    useEffect

--- watch/src/component/Onee/Onee.js
+++ watch/src/component/Onee/Onee.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Onee.css";
 import a from "../test/data";
 
+
 export default function Onee() {
+  let [alert,setAlert] = useState(true)
+
+  useEffect(()=>{
+    setTimeout(()=>{setAlert(false)},2000)
+  },)
   return (
     <div>
         <div className='main-bg'></div>
 
-        <div className='container'>
+        {/* <div className='container'>
             <row>
             <div className='col-md-4'>안녕</div>
             <img src='https://codingapple1.github.io/shop/shoes1.jpg'width="20%"></img>
@@ -22,7 +28,16 @@ export default function Onee() {
             <h4>상품명</h4>
             <p>상품설명</p>
             </row>
-        </div>
+        </div> */}
+{
+  alert == true
+  ? <div>
+  <button className='gd'>ㅎㅇ</button>
+</div>
+:null
+}
+
+        
     </div>
   )
 }
