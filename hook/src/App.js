a5094ac540ece8cca1e10ece22d5eaf6264b3e78
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-10-02 17:52:19 +0900

    useTitle

--- hook/src/App.js
+++ hook/src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 // import Input from "./component/Hook2/Input";
 // import Hook1 from "./component/Hook1/Hook1";
 import Tab from "./component/Hook3/Tab";
+import Title from "./component/Hook4/Title";
 
 function App() {
   return (
     <div className="App">
       {/* <Hook1 /> */}
       {/* <Input /> */}
-      <Tab />
+      {/* <Tab /> */}
+      <Title />
     </div>
   );
 }
