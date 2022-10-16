287a74b5b6dfb3801a9a2a509c63a2c4c0e7d107
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-10-16 14:07:29 +0900

    아 몰랑

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
+
 // import Input from "./component/Hook2/Input";
 // import Hook1 from "./component/Hook1/Hook1";
 // import Tab from "./component/Hook3/Tab";
 // import Title from "./component/Hook4/Title";
-import References from "./component/Hook5/References";
 import Click from "./component/Hook6/Click";
 import BeforeLeave from "./component/Hook7/BeforeLeave";
 import Fadeln from "./component/Hook8/Fadeln";
 import Scroll from "./component/Hook9/Scroll";
+import Request from "./component/Hook10/Request";
 
 function App() {
   return (
@@ -20,7 +21,8 @@ function App() {
       {/* <Click /> */}
       {/* <BeforeLeave /> */}
       {/* <Fadeln /> */}
-      <Scroll />
+      {/* <Scroll /> */}
+      <Request />
     </div>
   );
 }
