fa2eefce3a39e4472a33899d325a5e6ecbe6df90
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-11-28 21:40:55 +0900

    styled-component

---
+++ styled-component/src/components/StyledComponentsExample/StyledComponentsExample.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+export default function StyledComponentsExample() {
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
+  return (
+    <Wrapper>
+      <Title>Hello World!</Title>
+    </Wrapper>
+  );
+}
