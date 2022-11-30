15c211508ca6431870f6e53dd410142a7272eefc
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-11-30 19:32:41 +0900

    styled-component

--- styled-component/src/components/StyledComponentsExample/StyledComponentsExample.jsx
+++ styled-component/src/components/StyledComponentsExample/StyledComponentsExample.jsx
@@ -11,9 +11,40 @@ export default function StyledComponentsExample() {
     padding: 4em;
     background: papayawhip;
   `;
+
+  const Button = styled.button`
+    /* Adapt the colors based on primary prop */
+    background: ${(props) => (props.primary ? "palevioletred" : "white")};
+    color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+  `;
+
+  const TomatoButton = styled(Button)`
+    color: tomato;
+    border-color: tomato;
+  `;
   return (
-    <Wrapper>
-      <Title>Hello World!</Title>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
+      <Button onClick={() => alert("normal")}>Normal</Button>
+      <Button onClick={() => alert("primary")} primary>
+        Primary
+      </Button>
+      <TomatoButton>Tomato</TomatoButton>
+      <br />
+      <Button as="a" href="#">
+        Link with Button styles
+      </Button>
+      <TomatoButton as="a" href="#">
+        Link with Tomato Button styles
+      </TomatoButton>
+    </>
   );
 }
