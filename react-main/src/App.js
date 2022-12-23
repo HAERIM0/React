c0f57acd6e5129fd6bf44863203f2266d95a4f8f
전해림 <101709981+HAERIM0@users.noreply.github.com>
2022-12-23 11:15:01 +0900

    ref

--- react-main/src/App.js
+++ react-main/src/App.js
@@ -2,24 +2,26 @@ import React, { Component } from "react";
 // import Counter from "./Component/Counter";
 import Counter from "./Component/Counter";
 import Say from "./Component/Say";
-import MyComponent from "./MyConponent";
+import MyComponent from "./Component/MyConponent";
 import Event from "./Component/Event";
-const App = () => {
-  return <MyComponent name="React" />;
-};
+import ValidationSample from "./Component/ValidationSample/ValidationSample";
+// const App = () => {
+//   return <MyComponent name="React" />;
+// };
 
-const App = () => {
-  return <MyComponent />;
-};
+// const App = () => {
+//   return <MyComponent />;
+// };
 
 const App = () => {
   return (
-    <MyComponent name="react" favoriteNumber={7}>
-      리액트
-    </MyComponent>
-    <Counter />
-    <Say />
-    <Event />
+    // <MyComponent name="react" favoriteNumber={7}>
+    //   리액트
+    // </MyComponent>
+    // <Counter />
+    // <Say />
+    // <Event />
+    <ValidationSample />
   );
 };
 export default App;
